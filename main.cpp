feb2235d31dbd5ddcc732a3dfd5603cf226ee330
hansmu <hans.miikael@gmail.com>
2019-06-25 18:31:35 +0300

    Initial commit with variable declaration examples and why {} is better than =

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void variableExamples();
+
+int main() {
+    variableExamples();
+
+    return 0;
+}
+
+void variableExamples() {
+    // Three different ways to initialize an int
+    int age = 21;
+    int constructorInt (22);
+    int arrayLikeInt {23};
+
+    unsigned short examScore {55};
+    long populationNumber {1234125125};
+    // If we would've used = initialization then if the variable would've been a long we would've got an overflow but the
+    // program would have compiled. With {} we got a compilation error instead.
+    long long peopleOnEarth {7'600'000'000}; // ' can be used for formatting to have the number more easily readable
+
+    float rate {42.2};
+    double someNumber {3.121421241};
+    long double largeAmount {2.7e120};
+
+    bool isGameOver {false};
+
+    char initial {'J'};
+
+    short value1 {3000};
+
+    cout << "Hello, World!" << endl;
+    cout << age << " " << constructorInt << " " << arrayLikeInt << endl;
+
+    const double pricePerRoom {30};
+    const double salesTax {0.06};
+    const int estimateExpirationDays {30};
+}
